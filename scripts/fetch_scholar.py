ae81080906cc7154e920d86911841d7c0f1d74c7
simanirakiza <simanirakiza@umass.edu>
2025-02-01 04:28:07 -0500

    Update fetch_scholar.py

--- scripts/fetch_scholar.py
+++ scripts/fetch_scholar.py
@@ -15,6 +15,7 @@ for pub in author['publications']:
     pub_details = {
         "title": pub.get('bib', {}).get('title', 'N/A'),
         "year": pub.get('bib', {}).get('pub_year', 'N/A'),
+        "venue": pub.get('bib', {}).get('citation', 'N/A'),
         "link": f"https://scholar.google.com/citations?view_op=view_citation&hl=en&user={scholar_id}&citation_for_view={pub['author_pub_id']}"
     }
     publications.append(pub_details)
